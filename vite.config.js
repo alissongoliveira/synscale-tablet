116efe7a4f19cebed659bd5e2625832d3debb14f
Alisson G. <eng.alissongabriel@gmail.com>
2025-05-30 14:26:21 -0300

    preparação do vite para atuar com PWA

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.png", "favicon.ico"],
+      manifest: {
+        name: "SynScale Tablet",
+        short_name: "SynScale",
+        description: "Sistema de acompanhamento de carga em balança",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#f1f5f9",
+        theme_color: "#1e293b",
+        icons: [
+          {
+            src: "logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
